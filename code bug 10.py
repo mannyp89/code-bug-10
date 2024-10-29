f2f8417689330f603381431f18b3e24190d70cf6
mannyp89 <manny.pulgarin89@gmail.com>
2024-10-29 11:25:36 -0400

    Add files via upload

---
+++ code bug 10.py
@@ -0,0 +1,13 @@
+def first_character(text):
+    if text:
+        print(text[0])  # This prints the first character
+    else:
+        print("Empty String")
+
+if __name__ == "__main__":
+    first_character("python")
+    first_character("yellow")
+    first_character("tomorrow")
+    first_character("heliotrope")
+    first_character("open")
+    first_character("name")
